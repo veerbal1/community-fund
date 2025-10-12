06afd231813c946c1bc1a3f36a840dadfe4769cd
veerbal1 <veerbalsingh1@gmail.com>
2025-10-12 20:19:48 +0530

    Add user profile management to community fund program
    - Introduced Proposal and UserProfile account structures to manage proposals and user data.
    - Implemented ProposalStatus enum for tracking proposal states.
    - Updated the initialize function to create user profiles with initial values.
    - Enhanced tests to verify user profile initialization and state management.

--- programs/community-fund/src/lib.rs
+++ programs/community-fund/src/lib.rs
@@ -2,15 +2,51 @@ use anchor_lang::prelude::*;
 
 declare_id!("6gE2epaU3z6ySCsnwY9fvWyCCTnUMZ97c4jkzvPg52St");
 
+#[account]
+pub struct Proposal {
+    pub id: u64,
+    pub owner: Pubkey,
+    pub title: String,
+    pub description: String,
+    pub amount_requested: u64,
+    pub status: ProposalStatus,
+    pub created_at: i64,
+    pub vote_count: u64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub enum ProposalStatus {
+    Pending,
+    Approved,
+    Rejected,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct UserProfile {
+    pub proposal_count: u64,
+    pub bump: u8,
+}
+
 #[program]
 pub mod community_fund {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_user(ctx: Context<InitializeUser>) -> Result<()> {
+        let user_profile = &mut ctx.accounts.user_profile;
+        user_profile.proposal_count = 0;
+        user_profile.bump = ctx.bumps.user_profile;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeUser<'info> {
+    #[account(init, seeds=[b"user_profile", user.key().as_ref()], bump, payer = user , space = 8 + UserProfile::INIT_SPACE)]
+    pub user_profile: Account<'info, UserProfile>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
--- tests/community-fund.ts
+++ tests/community-fund.ts
@@ -1,16 +1,31 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CommunityFund } from "../target/types/community_fund";
+import { PublicKey } from "@solana/web3.js";
+import { expect } from "chai";
+
+const generatePDA = (key: PublicKey, programId: PublicKey) => {
+  return PublicKey.findProgramAddressSync(
+    [Buffer.from("user_profile"), key.toBuffer()],
+    programId
+  )[0];
+};
 
 describe("community-fund", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.getProvider();
+  const user = provider.wallet.publicKey;
+  
   const program = anchor.workspace.communityFund as Program<CommunityFund>;
+  const userProfilePDA = generatePDA(user, program.programId);
 
-  it("Is initialized!", async () => {
+  it("Initialize user", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initializeUser().rpc();
+    console.log("Transaction signature", tx);
+
+    const userProfile = await program.account.userProfile.fetch(userProfilePDA);
+    expect(userProfile.proposalCount.toNumber()).to.equal(0);
   });
 });
